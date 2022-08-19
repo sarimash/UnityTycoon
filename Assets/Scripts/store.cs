85a2a3db8c82a67d111512067065292f59d2e383
sarimash <brandon.stickle@gmail.com>
2022-08-18 23:30:45 -0700

    Update UI with total money; naive approach

--- Assets/Scripts/store.cs
+++ Assets/Scripts/store.cs
@@ -7,16 +7,27 @@ using TMPro;
 public class store : MonoBehaviour
 {
 
+    float CurrentBalance;
+    float BaseStoreCost;
+
     int storeCount;
     public GameObject StoreCountText;
+    public GameObject CurrentBalanceText;
+
 
     // Start is called before the first frame update
     void Start()
     {
         // set storeCount to 1
         storeCount = 1;
+        // set current balance to $2.00
+        CurrentBalance = 2.00f;
+        // set BaseStoreCost to $1.00
+        BaseStoreCost = 1.00f;
+
         // set the text of StoreCountText to storeCount
         StoreCountText.GetComponent<TextMeshProUGUI>().text = storeCount.ToString();
+        CurrentBalanceText.GetComponent<TextMeshProUGUI>().text = "$" + CurrentBalance.ToString("0.00");
 
     }
 
@@ -27,9 +38,18 @@ public class store : MonoBehaviour
     }
     
     public void BuyStoreOnClick () {
-        storeCount++;
-        //set the text of StoreCountText to storeCount
-        StoreCountText.GetComponent<TextMeshProUGUI>().text = storeCount.ToString();
+        if (BaseStoreCost > CurrentBalance) {
+            // do nothing
+        } else {
+            // add 1 to storeCount
+            storeCount++;
+            // add BaseStoreCost to CurrentBalance
+            CurrentBalance -= BaseStoreCost;
+            // set the text of StoreCountText to storeCount
+            StoreCountText.GetComponent<TextMeshProUGUI>().text = storeCount.ToString();
+            // update CurrentBalanceText to CurrentBalance
+            CurrentBalanceText.GetComponent<TextMeshProUGUI>().text = "$" + CurrentBalance.ToString("0.00");
+        }
     }
 
 }
